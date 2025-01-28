ef757b4f155219dbe809f3fe0d3b40e93b75e0f3
Roman Scherbakow <rip19880@gmail.com>
2025-01-28 23:05:30 +0300

    Начало реализации алгоритма Коэна-Сазерленда.

---
+++ GIS_WPF/Data/Primitives/Line2D.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIS_WPF.Data.Primitives
+{
+    internal class Line2D
+    {
+        public Point2D P1 { get; set; }
+        public Point2D P2 { get; set; }
+
+        public int line { get; set; } = 0;
+
+        // Линия внутри viewports 
+        // 0 - null - Не инициализирована
+        // 1 - Внутри
+        // 2 - Не пересекает
+        // 3 - Возможно пересекает
+
+        public string descr { get; set; } = "";
+
+        public Line2D(Point2D p1,Point2D p2)
+        {
+            P1 = p1;
+            P2 = p2;
+        }
+    }
+}
--- GIS_WPF/MainWindow.xaml.cs
+++ GIS_WPF/MainWindow.xaml.cs
@@ -21,59 +21,164 @@ namespace GIS_WPF
         /*
          * Координаты ViewPort
          */
-        int Xmin = 50;
-        int Ymin = 50;
-        int Xmax = 750;
-        int Ymax = 350;
+        int Xmin = 350;
+        int Ymin = 150;
+        int Xmax = 1500;
+        int Ymax = 750;
 
-        List<Point2D> viewport = new List<Point2D>();
-        List<Point2D> buffer   = new List<Point2D>();
+        List<Point2D> viewport_points = new List<Point2D>();
+        List<Point2D> buffer_points   = new List<Point2D>();
+
+        List<Line2D> viewport_lines = new List<Line2D>();
+        List<Line2D> buffer_lines   = new List<Line2D>();
         public MainWindow()
         {
-            viewport.Add(new Point2D(Xmin, Ymin));
-            viewport.Add(new Point2D(Xmax, Ymin));
-            viewport.Add(new Point2D(Xmax, Ymax));
-            viewport.Add(new Point2D(Xmin, Ymax));
+            viewport_points.Add(new Point2D(Xmin, Ymin));
+            viewport_points.Add(new Point2D(Xmax, Ymin));
+            viewport_points.Add(new Point2D(Xmax, Ymax));
+            viewport_points.Add(new Point2D(Xmin, Ymax));
 
-            buffer.Add(new Point2D(150, 30));
-            buffer.Add(new Point2D(250, 200));
-            buffer.Add(new Point2D(10,  200));
+            viewport_lines.Add(new Line2D(viewport_points[0], viewport_points[1]));
+            viewport_lines.Add(new Line2D(viewport_points[1], viewport_points[2]));
+            viewport_lines.Add(new Line2D(viewport_points[2], viewport_points[3]));
+            viewport_lines.Add(new Line2D(viewport_points[3], viewport_points[0]));
+
+            buffer_points.Add(new Point2D(330, 30));
+            buffer_points.Add(new Point2D(850, 600));
+            buffer_points.Add(new Point2D(300,  600));
+
+
+            buffer_lines.Add(new Line2D(buffer_points[0], buffer_points[1]));
+            buffer_lines.Add(new Line2D(buffer_points[1], buffer_points[2]));
+            buffer_lines.Add(new Line2D(buffer_points[2], buffer_points[0]));
 
             InitializeComponent();
+            Check_Lines();
             Draw();
+        }
+
+        public int getXorY(Point2D point)
+        {
+            int number = 0;
+            if (point.X < Xmin)
+                number = 1;
+            else
+                if (point.X > Xmax)
+                number = 2;
+
+            if (point.Y < Ymin)
+                number = number | 8;
+            else
+                if (point.Y > Ymax)
+                number = number | 4;
+
+            return number;
+        }
+        private void Check_Lines()
+        {
+            byte LEFT_OF_VIEWPORT  = 1; // левее
+            byte RIGHT_OF_VIEWPORT = 2; // правее
+            byte ABOVE_OF_VIEWPORT = 4; // выше
+            byte BELOW_OF_VIEWPORT = 8; // ниже
+
+            int[] tmp= new int[3];
+            int iterrator = 0;
+
+            //foreach(var pt in viewport_lines)
+            for (int i = 0; i < buffer_lines.Count; i++) 
+            {
+
+                int num1 = getXorY(buffer_lines[i].P1);
+                int num2 = getXorY(buffer_lines[i].P2);
+                if ((num1 == 0) && (num2 == 0))
+                {
+                    buffer_lines[i].descr = "Inside ViewPort";
+                    buffer_lines[i].line = 1;
+                }
+                else
+                    if ((num1 & num2) != 0)
+                {
+                    buffer_lines[i].descr = "Не пересекает ViewPort";
+                    buffer_lines[i].line = 2;
+
+                }
+                else
+                    if ((num1 & num2) != 0)
+                {
+                    buffer_lines[i].descr = "Возможно пересекает ViewPort";
+                    buffer_lines[i].line = 3;
+
+                }
+            }
+
+            //foreach(var pt in buffer_points)
+            //{
+            //    if (pt.X < Xmin)
+            //        tmp[iterrator] = 1;
+            //    else
+            //        if (pt.X > Xmax)
+            //        tmp[iterrator] = 2;
+
+            //    if (pt.Y < Ymin)
+            //        tmp[iterrator] = tmp[iterrator] | 8;
+            //    else
+            //        if(pt.Y > Ymax)
+            //        tmp[iterrator] = tmp[iterrator] | 4;
+
+            //    iterrator++;    
+            //}
         }
 
         private void Draw()
         {
-            for (int i = 0; i < viewport.Count; i++)
+            foreach(var pt in viewport_lines)
             {
                 Line line = new Line();
-                line.X1 = viewport[i].X;
-                line.Y1 = viewport[i].Y;
-                line.X2 = viewport[(i+1)%viewport.Count].X;
-                line.Y2 = viewport[(i+1)%viewport.Count].Y;
+                line.X1 = pt.P1.X;
+                line.Y1 = pt.P1.Y;
+                line.X2 = pt.P2.X;
+                line.Y2 = pt.P2.Y;
                 line.Stroke = Brushes.Black;
                 grid1.Children.Add(line);
-
             }
+            //for (int i = 0; i < viewport_points.Count; i++)
+            //{
+            //    Line line = new Line();
+            //    line.X1 = viewport_points[i].X;
+            //    line.Y1 = viewport_points[i].Y;
+            //    line.X2 = viewport_points[(i + 1) % viewport_points.Count].X;
+            //    line.Y2 = viewport_points[(i + 1) % viewport_points.Count].Y;
+            //    line.Stroke = Brushes.Black;
+            //    grid1.Children.Add(line);
 
-            for(int i = 0; i < buffer.Count; i++)
+            //}
+
+            //for (int i = 0; i < buffer_points.Count; i++)
+            foreach(var pt in buffer_lines)
             {
                 Line line = new Line();
-                line.X1 = buffer[i].X;
-                line.Y1 = buffer[i].Y;
-                line.X2 = buffer[(i + 1) % buffer.Count].X;
-                line.Y2 = buffer[(i + 1) % buffer.Count].Y;
+                //line.X1 = buffer_points[i].X;
+                //line.Y1 = buffer_points[i].Y;
+                //line.X2 = buffer_points[(i + 1) % buffer_points.Count].X;
+                //line.Y2 = buffer_points[(i + 1) % buffer_points.Count].Y;
+
+                line.X1 = pt.P1.X;
+                line.Y1 = pt.P1.Y;
+                line.X2 = pt.P2.X;
+                line.Y2 = pt.P2.Y;
                 line.Stroke = Brushes.Red;
+
+                if (pt.line == 1) 
+                    line.Stroke = Brushes.Purple;
+                else
+                    if (pt.line == 2)
+                    line.Stroke = Brushes.Green;
+                else
+                    if (pt.line == 3)
+                    line.Stroke = Brushes.Blue;
+
                 grid1.Children.Add(line);
             }
-            //Line vertL = new Line();
-            //vertL.X1 = 10;
-            //vertL.Y1 = 150;
-            //vertL.X2 = 10;
-            //vertL.Y2 = 10;
-            //vertL.Stroke = Brushes.Black;
-            //grid1.Children.Add(vertL);
         }
     }
 }
